d578b26921ef269e6c5a174313fda8bbdd6a7180
sauldnn <saul.dnn7@gmail.com>
2021-12-02 03:26:40 -0600

    detokenize sentences

--- neural_machine_translation/machine_translation.py
+++ neural_machine_translation/machine_translation.py
@@ -89,4 +89,32 @@ def tokenize(input_str, vocab_file=None, vocab_dir=None):
 
     # Set the encoding of the "end of the sentence" as 1
     EOS = 1
-    
+
+    inputs = next(trax.data.tokenize(iter([input_str]),
+                                    vocab_file=vocab_file,
+                                    vocab_dir=vocab_dir
+                                    ))
+    inputs = inputs + [EOS]
+    batch_inputs = np.reshape(np.array(inputs), [1, -1])
+    return = batch_inputs
+
+def detokenize(integers, vocab_file=None, vocab_dir=None):
+    """Decodes an array of integer to a human readable string
+
+    Args:
+        integers (numpy.ndarray): array of integers to decode
+        vocab_file (str): filename of the vocabulary text file
+        vocab_dir (str): path to the vocabulary file
+
+    Returns:
+        str: the decoded sentence
+    """
+
+    integers = list(np.squeeze(integers))
+
+    EOS=1
+
+    if EOS ni integers:
+        integers = integers[: integers.index(EOS)]
+
+    return trax.data.detokenize(integers, vocab_file=vocab_file, vocab_dir, vocab_dir)
