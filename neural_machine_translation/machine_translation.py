c68cef57f2df604e3cc57ad7090dc38098407fa1
sauldnn <saul.dnn7@gmail.com>
2021-12-02 02:57:28 -0600

    tokenize sentences

--- neural_machine_translation/machine_translation.py
+++ neural_machine_translation/machine_translation.py
@@ -66,5 +66,27 @@ tokenized_eval_stream = append_eos(tokenized_eval_stream)
 
 filtered_train_stream = trax.data.FilterByLength(
                         max_length=512,
-                        length_keys=
-)
+                        length_keys=[0, 1]
+                        )(tokenized_eval_stream)
+
+train_input, train_target = next(filtered_train_stream)
+# print a sample niput-target pair o tokenized sentences
+# print(colored(f'Single tokenized example input:'), train_input)
+# print(colored(f'Single tokenized example target:'), train_target)
+
+# setup helper functions for tokenizing and detokenizing sentences
+
+def tokenize(input_str, vocab_file=None, vocab_dir=None):
+    """Encodes a string to an array of integers
+    Inputs:
+        input_str (str): huma-readable string to encode
+        vocab_file (str): filename of the vocabulary text file
+        vocab_dir (str): path to the vocabulary file
+
+    Returns:
+        numpy.ndarray: tokenized version of the input string
+    """
+
+    # Set the encoding of the "end of the sentence" as 1
+    EOS = 1
+    
